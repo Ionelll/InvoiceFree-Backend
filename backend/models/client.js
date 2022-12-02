1023398e3d705ec0b389af0c2ee86e43e5f90027
marius <marius.pap14@gmail.com>
2022-12-02 17:08:34 +0200

    added get clients and get client after name

--- backend/models/client.js
+++ backend/models/client.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
+
 const clientSchema = new mongoose.Schema({
   nume: { type: String, require: true },
   adresa: { type: String, require: true },
   telefon: { type: String, require: true },
   cui: { type: String },
-  email: { type: String },
+  email: { type: String , unique:true},
   website: { type: String },
 });
+
 
 module.exports = mongoose.model("Client", clientSchema);
