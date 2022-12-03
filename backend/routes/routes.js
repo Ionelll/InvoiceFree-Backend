3b9b1a0628079cd6e709e9c7379c7b18ce1982c2
marius <marius.pap14@gmail.com>
2022-12-03 15:24:58 +0200

    delete client, update client done

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,7 @@
 const Client = require("../models/client");
 const express = require("express");
 const router = express.Router();
-
+const ObjectId = require("mongo-objectid");
 //router.post folosesti, ok(Mariuss)
 
 router.post("/addclient", (req, res) => {
@@ -13,19 +13,7 @@ router.post("/addclient", (req, res) => {
     email: req.body.email,
     website: req.body.website,
   });
-  if((clientnou.nume == null || clientnou.nume == "" || typeof(clientnou.nume) == 'undefined' )  || 
-    (clientnou.adresa == null || clientnou.adresa == "" || typeof(clientnou.adresa) == 'undefined') || 
-    (clientnou.telefon == null || clientnou.telefon == "" || typeof(clientnou.telefon) == 'undefined') || 
-    (clientnou.cui == null || clientnou.cui == "" || typeof(clientnou.cui) == 'undefined') || 
-    (clientnou.email == null || clientnou.email == "" || typeof(clientnou.email) == 'undefined') || 
-    (clientnou.website == null || clientnou.website == "" || typeof(clientnou.website) == 'undefined')){
-    res.status(403).json({
-      message: "All fields are required",
-      result: []
-    })
-    
-  }
-  else{
+  
     clientnou
     .save()
     .then((result) => {
@@ -39,11 +27,11 @@ router.post("/addclient", (req, res) => {
         error: error,
       });
     });
-  }
+  
   
 });
 
-router.use("/users", (req, res) => {
+router.use("/clients", (req, res) => {
 
   Client.find().then((result) => {
     res.status(200).json({
@@ -59,7 +47,7 @@ router.use("/users", (req, res) => {
 });
 
 
-router.use("/user/:nume", (req, res) => {
+router.use("/client/:nume", (req, res) => {
 
   Client.find({nume: req.params.nume}).then((result) => {
     // console.log(result)
@@ -77,6 +65,94 @@ router.use("/user/:nume", (req, res) => {
 
 
 
+router.post("/deleteClient", (req, res) => {
+  let Id = new ObjectId("");
+  id = req.body._id
+  Client.deleteOne({_id: id}).then((result)=>{
+    if(result.deletedCount < 1){
+      res.status(401).json({
+        message: "cant delete client",
+      });
+    }
+    else{
+      res.status(200).json({
+        message: "Item was deleted succesfully",
+      });
+    }
+  })
+})
+
+
+router.post("/updateClient", (req,res)=> {
+  
+  let Id = new ObjectId("");
+  id = req.body._id
+  Client.findOne({_id: id}).then((result)=>{
+    if(req.body.nume === null || req.body.nume === "" || typeof(req.body.nume) === undefined){
+      var nume = result.nume
+    }
+    else{
+      var nume = req.body.nume
+    }
+
+    if(req.body.adresa === null || req.body.adresa === "" || typeof(req.body.adresa) === undefined){
+      var adresa = result.adresa
+    }
+    else{
+      var adresa = req.body.adresa
+    }
+
+    if(req.body.telefon === null || req.body.telefon === "" || typeof(req.body.telefon) === undefined){
+      var telefon = result.telefon
+    }
+    else{
+      var telefon = req.body.telefon
+    }
+
+    if(req.body.cui === null || req.body.cui === "" || typeof(req.body.cui) === undefined){
+      var cui = result.cui
+    }
+    else{
+      var cui = req.body.cui
+    }
+
+    if(req.body.email === null || req.body.email === "" || typeof(req.body.email) === undefined){
+      var email = result.email
+    }
+    else{
+      var email = req.body.email
+    }
+
+    if(req.body.website === null || req.body.website === "" || typeof(req.body.website) === undefined){
+      var website = result.website
+    }
+    else{
+      var website = req.body.website
+    }
+
+    const  updateClient = ({
+      nume: nume,
+      adresa: adresa,
+      telefon: telefon,
+      cui: cui,
+      email: email,
+      website: website,
+    });
+
+   
+    Client.findByIdAndUpdate({_id: id}, updateClient, (error, data) => {
+      if (error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json(data); 
+      }
+    }
+    );
+
+   
+  })
+
+})
 
 
 
