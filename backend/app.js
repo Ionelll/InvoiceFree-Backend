74f3abfa97f435b36b90b233a3b2ff7f11785bea
marius <marius.pap14@gmail.com>
2022-12-08 23:38:27 +0200

    front+back

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const router = require("./routes/routes.js");
+const router = require("./routes/client.js");
+const send = require("./routes/facturi.js")
+const fileUpload = require("express-fileupload")
 
 app = express();
 
@@ -13,10 +15,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/Facturi", function (err) {
   }
 });
 
-app.use(express.json({ limit: "2mb", extended: "true" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: "http://localhost:4200" }));
 app.use(express.static("facturi"));
+app.use(fileUpload())
+
 app.use("/api", router);
+app.use("/api/invoice", send);
 
 module.exports = app;
--- backend/models/client.js
+++ backend/models/client.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
+
 const clientSchema = new mongoose.Schema({
   nume: { type: String, required: true },
   adresa: { type: String, required: true },
   telefon: { type: String, required: true },
   cui: { type: String },
-  email: { type: String },
+  email: { type: String , unique:true},
   website: { type: String },
 });
+
 
 module.exports = mongoose.model("Client", clientSchema);
