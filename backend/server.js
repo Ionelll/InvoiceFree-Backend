56613eed8bed8b52060822bae613e455d24a2f41
Ionelll <serban.nelutu@gmail.com>
2023-12-11 10:20:35 +0100

    trying

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const http = require("http");
 
-const server = app.listen(process.env.PORT || 3000);
+const server = http.createServer(app).listen(process.env.PORT || 3000);
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
